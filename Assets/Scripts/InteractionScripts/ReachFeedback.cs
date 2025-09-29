a182fdceefe731f81b761bab7b69f90ceeab915f
Marchel <m.herreravsquez@outlook.com>
2025-09-29 10:21:06 -0300

    Update ReachFeedback.cs

--- Assets/Scripts/InteractionScripts/ReachFeedback.cs
+++ Assets/Scripts/InteractionScripts/ReachFeedback.cs
@@ -1,17 +1,40 @@
+using System;
 using UnityEngine;
 
 public class ReachFeedback : MonoBehaviour
 {
     public AudioSource instrumentOnReach;
+    bool inReach = false;
+
+    public Transform reachPivot;
+
+    private void Start()
+    {
+        reachPivot = GetComponentInChildren<Transform>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Instrument"))
         {
             instrumentOnReach = other.gameObject.GetComponent<AudioSource>();
+            instrumentOnReach.Play();
+            inReach = true;
             
             Debug.Log(other.name + " is in trigger");
             
+        }
+    }
+
+    private void Update()
+    {
+        if (inReach)
+        {
+            float distance = Vector3.Distance(reachPivot.position, instrumentOnReach.transform.position);
+            
+            
+            // Subir el volumen gradualmente mientras más cerca se encuentre del reachPivot
+            // instrumentOnReach.volume = 
         }
     }
 
@@ -19,7 +42,9 @@ public class ReachFeedback : MonoBehaviour
     {
         if (other.CompareTag("Instrument"))
         {
-            instrumentOnReach = other.gameObject.GetComponent<AudioSource>();
+            inReach  = false;
+            instrumentOnReach.Stop();
+            instrumentOnReach = null;
         }
     }
 }
